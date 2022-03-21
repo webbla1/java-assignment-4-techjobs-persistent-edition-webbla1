487bc591ecb9059d71083690a863d95a46a5455f
webbla1 <92279960+webbla1@users.noreply.github.com>
2022-03-20 22:31:47 -0500

    Added queries

--- queries.sql
+++ queries.sql
@@ -1,8 +1,18 @@
 -- Part 1: Test it with SQL
-
+CREATE TABLE techjobs ()
+    id int PRIMARY KEY,
+    employer VARCHAR(255),
+    name VARCHAR(255),
+    skills VARCHAR(255);
 -- Part 2: Test it with SQL
-SELECT * FROM techjobs.employer
-WHERE name = "Microsoft";
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
-
+DROP TABLE job;
 -- Part 4: Test it with SQL
+SELECT *
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
